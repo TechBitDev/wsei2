5c5f88b5bed735bf3884d5b30ab4cf3a78bc9be3
dwierzbicki11 <117636569+dwierzbicki11@users.noreply.github.com>
2025-03-14 18:25:56 +0100

    Audio

---
+++ Assets/scripts/Audio.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class Audio:MonoBehaviour
+    {
+        [SerializeField] AudioSource audioSource;
+        public AudioClip audioClip;
+
+        private void Start()
+        {
+            audioSource.clip = audioClip;
+            audioSource.Play();
+        }
+    }
+    
+}
